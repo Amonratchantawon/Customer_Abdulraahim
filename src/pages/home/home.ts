8cfeacdc91b1b28ec3a9f04efcd4ed29a4b59340
amonratCha <amonrat.cha@northbkk.ac.th>
2017-12-15 11:19:15 +0700

    aaaaaaaa

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { HomeProvider } from '../../providers/home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomeModel } from '../../assets/model/homeModel';
 
 @IonicPage()
 @Component({
@@ -8,16 +10,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
+  homeData:HomeModel = new HomeModel();
+
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    public home: HomeProvider
   ) {
-    
 
   }
 
-  ionViewDidLoad() {
+  ionViewWillEnter() {
     console.log('ionViewDidLoad HomePage');
+    this.getdata();
+  }
+
+  getdata() {
+    this.home.getAds().then(data => {
+      this.homeData = data;
+      console.log(this.homeData);
+    })
   }
 
 }

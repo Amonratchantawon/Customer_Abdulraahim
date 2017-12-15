0729d5c9bc6db64faf7fff1028d065f3e6d93b8e
amonratCha <amonrat.cha@northbkk.ac.th>
2017-12-15 10:54:56 +0700

    ass

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
-
 @IonicPage()
 @Component({
   selector: 'page-home',

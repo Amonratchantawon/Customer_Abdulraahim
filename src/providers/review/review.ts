d1461fa164049eedb8bf7fee62f9c9d5d5e6cbb6
amonratCha <amonrat.cha@northbkk.ac.th>
2017-12-17 13:15:41 +0700

    fffffffff

---
+++ src/providers/review/review.ts
@@ -0,0 +1,31 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReviewModel } from '../../assets/model/review';
+
+
+/*
+  Generated class for the ReviewProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ReviewProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ReviewProvider Provider');
+  }
+
+  getReviews(): Promise<Array<ReviewModel>> {
+    return this.http.get('./assets/json/review.json')
+      .toPromise()
+      .then(response => response as Array<ReviewModel>)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+  }
+
+}

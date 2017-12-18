146440d4fd898b61b1711e157e4af7fc1907df89
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-12-18 15:19:30 +0700

    update

--- src/pages/recommented/recommented.module.ts
+++ src/pages/recommented/recommented.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RecommentedPage } from './recommented';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { RecommentedPage } from './recommented';
   ],
   imports: [
     IonicPageModule.forChild(RecommentedPage),
+    ComponentsModule    
   ],
 })
 export class RecommentedPageModule {}

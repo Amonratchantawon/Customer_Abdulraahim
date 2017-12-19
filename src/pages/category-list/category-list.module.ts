0f26487d2f529bb3a7774691f704f3ec7edf9226
amonratCha <amonrat.cha@northbkk.ac.th>
2017-12-19 13:02:53 +0700

    ggggggg

--- src/pages/category-list/category-list.module.ts
+++ src/pages/category-list/category-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CategoryListPage } from './category-list';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CategoryListPage } from './category-list';
   ],
   imports: [
     IonicPageModule.forChild(CategoryListPage),
+    ComponentsModule
   ],
 })
 export class CategoryListPageModule {}

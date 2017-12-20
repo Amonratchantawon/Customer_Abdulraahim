e942adcc1f4274b63326082b28c0fc41bcec0eff
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-12-20 10:51:16 +0700

    update

--- src/pages/recommented/recommented.module.ts
+++ src/pages/recommented/recommented.module.ts
@@ -3,6 +3,7 @@ import { IonicPageModule } from 'ionic-angular';
 import { RecommentedPage } from './recommented';
 import { ComponentsModule } from '../../components/components.module';
 import { PipesModule } from '../../pipes/pipes.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -10,8 +11,9 @@ import { PipesModule } from '../../pipes/pipes.module';
   ],
   imports: [
     IonicPageModule.forChild(RecommentedPage),
+    TranslateModule.forChild(),
     ComponentsModule,
     PipesModule
   ],
 })
-export class RecommentedPageModule {}
+export class RecommentedPageModule { }

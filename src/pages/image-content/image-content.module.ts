42ab0ac4f7022923789baeab1e8d72ec2277e121
amonratCha <amonrat.cha@northbkk.ac.th>
2017-12-22 19:28:15 +0700

    fff

--- src/pages/image-content/image-content.module.ts
+++ src/pages/image-content/image-content.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ImageContentPage } from './image-content';
 
-// import * as ionicGalleryModal from 'ionic-gallery-modal';
-// import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-
-
 
 @NgModule({
   declarations: [

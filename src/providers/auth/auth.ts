f47ea684838fc051669c084a78e8eb2f2d501ff2
jigkoh3 <jigkoh3@hotmail.com>
2018-01-08 12:27:58 +0700

    xcx

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -49,7 +49,7 @@ export class AuthProvider {
   }
 
   private handleError(error: any): Promise<any> {
-    return Promise.reject(error.message || error);
+    return Promise.reject(error.error || error);
   }
 
 }
